d9711a49c3987d1ee19b11aea9b22f4abbd3288a
Alexandros <aaidonis1932@bths.edu>
2021-04-03 00:28:59 -0400

    Finished Milestone 1 and 2

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,8 +1,49 @@
 #include "Labyrinth.h"
+#include <iostream>
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
+	bool spellbook = false;
+	bool wand = false;
+	bool potion = false;
+	
+    for(int i = 0; i < moves.size(); i++)
+	{
+	    if(moves[i] == 'S')
+		{
+			start = start->south;
+		}
+		else if(moves[i] == 'E')
+		{
+			start = start->east;
+		}
+		else if(moves[i] == 'N')
+		{
+			start = start->north;
+		}
+		else if(moves[i] == 'W')
+		{
+			start = start->west;
+		}
+		if(start == nullptr)
+		{
+			return false;
+		}
+		if(start->whatsHere == Item::SPELLBOOK)
+		{
+			spellbook = true;
+		}
+		else if(start->whatsHere == Item::WAND)
+		{
+			wand = true;
+		}
+		else if(start->whatsHere == Item::POTION)
+		{
+			potion = true;
+		}
+	}
+	if(spellbook && wand && potion)
+	{
+		return true;
+	}
     return false;
 }
